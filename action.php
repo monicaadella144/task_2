731b517972924a162cec706d9292ec1ade5e7fdf
Monica Adella Aisyah Renaldi <94498496+monicaadella144@users.noreply.github.com>
2022-10-07 00:53:14 +0700

    Add files via upload

---
+++ action.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>RA_120140144_Monica Adella Aisyah Renaldi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Website Pembelian Handphone</h1>
+    <div class="bingkai">
+            <p><h3>Hasil Pencarian Handphone: </h3></p>
+            <p>Jenis Barang Handphone : <?=$_GET['jenis']?></p>
+            <p>Merk Barang Handphone : <?=$_GET['merk']?></p>
+    </div>   
+</body>
+</html>
